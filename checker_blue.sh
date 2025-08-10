531a3a0d7d240298975b0e736b2cf5ba1151d188
box7e7 <parislight@live.com>
2025-08-09 20:23:48 -0500

    chore: update code

--- checker_blue.sh
+++ checker_blue.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
 
+export PATH="/usr/local/bin:/usr/bin/:$PATH"
+
+export PUPPETEER_EXECUTABLE_PATH="/usr/bin/chromium"
+
 process_name="auth-bmw"
-result=$(pm2 list | grep "$process_name")
- 
+
+result=$(/usr/local/bin/pm2 list | grep "$process_name")
+
+echo $result
+
 if [[ -n $result ]]; then
+
   echo "true"
+
   pm2 delete $process_name
-  COLOR=blue pm2 start index.js --name auth-bmw --cwd /usr/src/app
+
+  COLOR=blue /usr/local/bin/pm2 start index.js --name auth-bmw --cwd /usr/src/app
+
 else
+
   echo "false"
+
 fi
